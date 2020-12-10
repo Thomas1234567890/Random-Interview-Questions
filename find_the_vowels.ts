c1e7c5a6c1bed4df1f8b92248cab0afe5dda3d8c
Thomas1234567890 <tompatcon@gmail.com>
2020-12-10 16:17:41 +0000

    1 file modified, 1 added(find_the_vowels)

---
+++ find_the_vowels.ts
@@ -0,0 +1,36 @@
+export {};
+////define
+
+//// return the number of vowels in a string
+
+////production
+function vowel_counter(entered_string: string, vowels: Array<string>) {
+  let counter = 0;
+  for (let char of entered_string) {
+    if (vowels.includes(char)) counter++;
+  }
+
+  return counter;
+}
+
+// //solution
+// const vowels = ['a', 'e', 'i', 'o', 'u'];
+// vowel_counter('hello',vowels);
+
+////test
+
+function test_vowel_counter(
+  entered_string: string,
+  vowels: Array<string>,
+  expected_value: number
+) {
+  let result = vowel_counter(entered_string, vowels);
+  return result === expected_value ? true : false;
+}
+
+function testcase() {
+  const vowels = ["a", "e", "i", "o", "u"];
+  console.log(test_vowel_counter("Hello", vowels, 2));
+}
+
+testcase();
--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@ import "./style.css";
 import "./fibonacci_sequence";
 import "./reverse_string";
 import "./fizzbuzz";
-import './multiples_question'
-import './anagram_question'
+import "./multiples_question";
+import "./anagram_question";
+import "./find_the_vowels";
 
 // console.log('index.ts loaded');
