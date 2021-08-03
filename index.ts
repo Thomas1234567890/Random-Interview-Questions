4c29888eb381f49fd5f4f0e11470581e525a6779
Thomas1234567890 <tompatcon@gmail.com>
2021-08-03 19:57:09 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,18 +1,14 @@
 import './style.css';
 
-import './fibonacci_sequence';
-import './reverse_string';
-import './fizzbuzz';
-import './multiples_question';
-import './anagram_question';
-import './find_the_vowels';
-import './array_methods';
-import {_first_recored_instance as answer, _confirmed_duplicates as duplicates, get_non_repeating_value as non_repeating, _confirmed_duplicates } from './non_repeating_values';
+// import './fibonacci_sequence';
+// import './reverse_string';
+// import './fizzbuzz';
+// import './multiples_question';
+// import './anagram_question';
+// import './find_the_vowels';
+// import './array_methods';
+// import {_first_recored_instance as answer, _confirmed_duplicates as duplicates, get_non_repeating_value as non_repeating, _confirmed_duplicates } from './non_repeating_values';
 
 // console.log('index.ts loaded');
-import './dropdown_button/_style.scss';
+// import './dropdown_button/_style.scss';
 console.clear();
-
-console.log(non_repeating('aaavccbcx'))
-console.log(answer)
-console.log(duplicates)
