c668d5926647fd4021c350b6d097d69cad805370
Thomas1234567890 <tompatcon@gmail.com>
2021-08-04 14:12:54 +0100

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import './style.css';
-
+import { log } from './_utility_methods';
 // import './fibonacci_sequence';
 // import './reverse_string';
 // import './fizzbuzz';
@@ -7,8 +7,17 @@ import './style.css';
 // import './anagram_question';
 // import './find_the_vowels';
 // import './array_methods';
-// import {_first_recored_instance as answer, _confirmed_duplicates as duplicates, get_non_repeating_value as non_repeating, _confirmed_duplicates } from './non_repeating_values';
+import {
+  _first_recored_instance as answer,
+  _confirmed_duplicates as duplicates,
+  get_non_repeating_value as non_repeating
+} from './non_repeating_values';
 
 // console.log('index.ts loaded');
 // import './dropdown_button/_style.scss';
 console.clear();
+
+non_repeating('aaabcccd');
+log(answer[0]);
+log(answer);
+log(duplicates);
